33f9940100540326c87aeb79ea0017e049775278
KaoriA <138273671+lilythelily@users.noreply.github.com>
2026-02-21 18:31:26 +0900

    updated

---
+++ js/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// === 📌 hamburger menu control ===
+
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const closeBtn = document.querySelector('.hamburger-close');
+
+function toggleHamburger(item) {
+    item.classList.toggle('slide-in');
+
+}
+
+hamburgerIcon.addEventListener('click', () => {
+    toggleHamburger(hamburgerMenu);
+})
+
+closeBtn.addEventListener('click', () => {
+    toggleHamburger(hamburgerMenu);
+})
+
+// === 📌 checkbox control ===
+const checkboxes = document.querySelectorAll(".checkbox");
+
+checkboxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        box.classList.toggle('unchecked');
+    })
+})
+
+// === 📌 thanks popup display control ===
+const thanksModal = document.querySelector('.thanks-modal');
+const modalClose = document.querySelector('.modal-close');
+const submitBtn = document.querySelector('.contact-btn');
+
+function toggleDisplay(item) {
+    item.classList.toggle('hide');
+    item.classList.toggle('flex');
+}
+
+submitBtn.addEventListener('click', () => {
+    toggleDisplay(thanksModal);
+})
+
+modalClose.addEventListener('click', () => {
+    toggleDisplay(thanksModal);
+})
